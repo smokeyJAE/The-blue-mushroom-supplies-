27d675dc799315a43c93dbb30bf197257fc5cbe9
smokeyJAE <smokey@thebluemushroomsupplies.com>
2025-05-05 09:21:35 -0600

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,1 +1,9 @@
+export default function handler(req, res) {
+  const products = [
+    { id: 1, name: "Syringe 10cc", price: 5.00 },
+    { id: 2, name: "Spore Print - Golden Teacher", price: 20.00 },
+    { id: 3, name: "Mason Jar w/ Lid", price: 8.00 }
+  ];
 
+  res.status(200).json(products);
+}
